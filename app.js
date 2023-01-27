53d359f7f8a5c5c1f782b969454c163faf37b297
Jaloliddin7 <abdutolipovjaloliddin913@gmail.com>
2023-01-27 22:34:08 +0500

    end

---
+++ app.js
@@ -0,0 +1,17 @@
+const burgerbtn = document.querySelector(".burger__btn");
+const menyubar = document.querySelector(".menyu__bar");
+const close = document.querySelector(".header__close");
+
+burgerbtn.addEventListener("click",()=> {
+    menyubar.classList.toggle("menyu__togle")
+    console.log("Salom");
+
+})
+close.addEventListener("click",()=> {
+    menyubar.classList.toggle("menyu__togle")
+})
+
+
+
+
+
